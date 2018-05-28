3d812f51d463530fe384e564a53df922bdc50a8e
James Andrews <bdaddyja@gmail.com>
2018-05-28 16:40:52 -0700

    Added test for nonexistingroutes.

--- main_test.go
+++ main_test.go
@@ -38,6 +38,36 @@ func TestRouter(t *testing.T) {
 
 }
 
+func TestRouterForNonExistentRoute(t *testing.T) {
+  r := newRouter()
+  mockServer := httptest.NewServer(r)
+
+  resp, err := http.Post(mockServer.URL+"/hello", "", nil)
+
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  if resp.StatusCode != http.StatusMethodNotAllowed {
+    t.Errorf("Status should be 405, got %d", resp.StatusCode)
+  }
+
+  defer resp.Body.Close()
+
+  b, err := ioutil.ReadAll(resp.Body)
+
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  respString := string(b)
+  expected := ""
+
+  if respString != expected {
+    t.Errorf("Response should be %s, got %s", expected, respString)
+  }
+}
+
 func TestHandler(t *testing.T) {
   req, err := http.NewRequest("GET", "", nil)
 
