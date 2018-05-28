a6cad4f689fe8ac40dec3e25879b25bc255d346c
James Andrews <bdaddyja@gmail.com>
2018-05-28 16:30:20 -0700

    Test routing.

--- main_test.go
+++ main_test.go
@@ -4,7 +4,40 @@ import (
   "net/http"
   "net/http/httptest"
   "testing"
+  "io/ioutil"
 )
+func TestRouter(t *testing.T) {
+  r := newRouter()
+
+  mockServer := httptest.NewServer(r)
+
+  resp, err := http.Get(mockServer.URL + "/hello")
+
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  if resp.StatusCode != http.StatusOK {
+    t.Errorf("Status should be ok, got %d", resp.StatusCode)
+  }
+
+  defer resp.Body.Close()
+
+  b, err := ioutil.ReadAll(resp.Body)
+
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  respString := string(b)
+  expected := "Hello World!"
+
+  if respString != expected {
+    t.Errorf("Response should be %s, got %s", expected, respString)
+  }
+
+}
+
 func TestHandler(t *testing.T) {
   req, err := http.NewRequest("GET", "", nil)
 
