e65105c9b025b97b957956bbbc999010350a7472
James Andrews <bdaddyja@gmail.com>
2018-05-28 16:12:33 -0700

    Added mux for routing.

--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
   "fmt"
   "net/http"
+  "github.com/gorilla/mux"
 )
 
+func newRouter() *mux.Router {
+  r := mux.NewRouter()
+  r.HandleFunc("/hello", handler).Methods("GET")
+
+  return r
+}
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+  r := newRouter()
+  http.ListenAndServe(":8080", r)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello World")
+  fmt.Fprintf(w, "Hello World!")
 }
