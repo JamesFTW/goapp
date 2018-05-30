87b0edd05fdd4ccb88bcb430d6560ca3cd78134d
James Andrews <bdaddyja@gmail.com>
2018-05-29 22:39:23 -0700

    Connect route handlers to database.

--- bird_handlers.go
+++ bird_handlers.go
@@ -14,6 +14,9 @@ type Bird struct {
 var birds []Bird
 
 func getBirdHandler(w http.ResponseWriter, r *http.Request) {
+
+  birds, err := store.GetBirds()
+
   birdListBytes, err := json.Marshal(birds)
 
   if err != nil {
@@ -39,8 +42,11 @@ func createBirdHandler(w http.ResponseWriter, r *http.Request) {
   bird.Species = r.Form.Get("species")
 	bird.Description = r.Form.Get("description")
 
-  birds = append(birds, bird)
-
+  err = store.CreateBird(&bird)
+  if err != nil {
+    fmt.Println(err)
+  }
+  
   http.Redirect(w, r, "/assets/", http.StatusFound)
 
 }
