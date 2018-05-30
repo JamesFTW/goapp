8b30e2423b9e12c1cfc2d1c7724cf38699b551db
James Andrews <bdaddyja@gmail.com>
2018-05-29 22:40:06 -0700

    Connecting to heroku database.

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
   "fmt"
   "net/http"
+  "database/sql"
   "github.com/gorilla/mux"
+  _ "github.com/lib/pq"
 )
 
 func newRouter() *mux.Router {
   r := mux.NewRouter()
-  
+
   r.HandleFunc("/hello", handler).Methods("GET")
   r.HandleFunc("/bird", getBirdHandler).Methods("GET")
 	r.HandleFunc("/bird", createBirdHandler).Methods("POST")
@@ -21,8 +23,24 @@ func newRouter() *mux.Router {
   return r
 }
 func main() {
-  r := newRouter()
-  http.ListenAndServe(":8080", r)
+  fmt.Println("Starting server...")
+	connString := "postgres://jduphbzynbfuch:0dc67026b814a1156e0854d22222557848e675929fa4b72c8716a63863751bab@ec2-23-23-130-158.compute-1.amazonaws.com:5432/dcomiopimevuq7"
+	db, err := sql.Open("postgres", connString)
+
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	InitStore(&dbStore{db: db})
+
+	r := newRouter()
+	fmt.Println("Serving on port 8080")
+	http.ListenAndServe(":8080", r)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
